f9e0e28776f1b6912bb1d9839dbf6f12b5824678
Jacobsolawetz <jacobsolawetz@gmail.com>
2022-04-19 13:39:14 -0500

    parse args add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![allow(unused)]
+
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    add: String,
+    /// The path to the file to read
+    description: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
