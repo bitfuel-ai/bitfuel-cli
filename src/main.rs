a530d95cb888bd3c05b100d46784ad156fe65468
Jacobsolawetz <jacobsolawetz@gmail.com>
2022-04-19 14:38:57 -0500

    basic subcommand structure

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,58 @@
-#![allow(unused)]
+use clap::Clap;
+use rand::Rng;
 
-use clap::Parser;
+/// This program can say 'hello' to you, show you 'quote' of the day
+/// and can even help you 'add' up numbers!
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Robin Wang<mytechtip@github>")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
 
-/// Search for a pattern in a file and display the lines that contain it.
-#[derive(Parser)]
-struct Cli {
-    /// The pattern to look for
-    add: String,
-    /// The path to the file to read
+#[derive(Clap)]
+enum SubCommand {
+    Save(Save),
+    Search(Search)
+}
+
+/// Say hello
+#[derive(Clap)]
+struct Save {
     description: String,
+    command: String
+}
+
+#[derive(Clap)]
+struct Search {
+    description: String
+}
+
+pub trait Display {
+    fn show(&self) -> String;
+}
+
+impl Display for Save {
+    fn show(&self) -> String {
+        format!("Saving {}...", self.description)
+    }
+}
+
+impl Display for Search {
+    fn show(&self) -> String {
+        format!("Searching {}...", self.description)
+    }
 }
 
 fn main() {
-    let args = Cli::parse();
+    let opts: Opts = Opts::parse();
+
+    match opts.subcmd {
+        SubCommand::Save(t) => {
+            println!("{}", t.show());
+        }
+        SubCommand::Search(t) => {
+            println!("{}", t.show());
+        }
+    }
 }
