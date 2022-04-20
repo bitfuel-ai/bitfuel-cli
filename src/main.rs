e40769916de4b2ab38620ade42f9e30a94e4b93a
jacobsolawetz <jacobsolawetz@gmail.com>
2022-04-20 18:40:20 +0000

    shippin with flags

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,28 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "toast", about = "The best place to stack your JAM")]
+#[structopt(name = "bashfull", about = "never forget a bash command again")]
 enum Bashfull {
+    /// Activate debug mode
+    // short and long flags (-d, --debug) will be deduced from the field's name
+
     Save {
-        #[structopt()]
+        #[structopt(short = "d", long = "description")]
         descript: String,
 
-        #[structopt()]
+        #[structopt(short = "c", long = "command")]
         command: String,
+
+    },
+    Query {
+        #[structopt(short = "d", long = "description")]
+        descript: String,
     },
 }
 
 fn main() {
     let opt = Bashfull::from_args();
+    print_type_of(&opt);
     println!("{:?}", opt);
+    //dbg!(opt);
 }
