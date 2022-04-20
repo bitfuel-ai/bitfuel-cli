2f42b9515a7e6fa657832961d1f94af8dd810035
jacobsolawetz <jacobsolawetz@gmail.com>
2022-04-20 03:44:23 +0000

    refactor with structopt

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,19 @@
-use clap::Clap;
-use rand::Rng;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-/// This program can say 'hello' to you, show you 'quote' of the day
-/// and can even help you 'add' up numbers!
-#[derive(Clap)]
-#[clap(version = "1.0", author = "Robin Wang<mytechtip@github>")]
-struct Opts {
-    #[clap(subcommand)]
-    subcmd: SubCommand,
-}
+#[derive(Debug, StructOpt)]
+#[structopt(name = "toast", about = "The best place to stack your JAM")]
+enum Bashfull {
+    Save {
+        #[structopt()]
+        descript: String,
 
-#[derive(Clap)]
-enum SubCommand {
-    Save(Save),
-    Search(Search)
-}
-
-/// Say hello
-#[derive(Clap)]
-struct Save {
-    description: String,
-    command: String
-}
-
-#[derive(Clap)]
-struct Search {
-    description: String
-}
-
-pub trait Display {
-    fn show(&self) -> String;
-}
-
-impl Display for Save {
-    fn show(&self) -> String {
-        format!("Saving {}...", self.description)
-    }
-}
-
-impl Display for Search {
-    fn show(&self) -> String {
-        format!("Searching {}...", self.description)
-    }
+        #[structopt()]
+        command: String,
+    },
 }
 
 fn main() {
-    let opts: Opts = Opts::parse();
-
-    match opts.subcmd {
-        SubCommand::Save(t) => {
-            println!("{}", t.show());
-        }
-        SubCommand::Search(t) => {
-            println!("{}", t.show());
-        }
-    }
+    let opt = Bashfull::from_args();
+    println!("{:?}", opt);
 }
