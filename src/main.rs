ff4c15816dca2a221a6f3c5e4fba63d091886805
jacobsolawetz <jacobsolawetz@gmail.com>
2022-04-20 22:37:00 +0000

    checkpoint with reqwests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -6,7 +5,7 @@ use structopt::StructOpt;
 enum Bashfull {
     /// Activate debug mode
     // short and long flags (-d, --debug) will be deduced from the field's name
-
+    #[structopt(name = "save")]
     Save {
         #[structopt(short = "d", long = "description")]
         descript: String,
@@ -15,15 +14,65 @@ enum Bashfull {
         command: String,
 
     },
+    #[structopt(name = "query")]
     Query {
         #[structopt(short = "d", long = "description")]
         descript: String,
     },
 }
 
-fn main() {
-    let opt = Bashfull::from_args();
-    print_type_of(&opt);
-    println!("{:?}", opt);
-    //dbg!(opt);
-}
+// async fn save(descript:String, command:String) -> Result<() >> {
+//     let client = reqwest::Client::new();
+//     let resp = client.get("http://api.roboflow.com")
+//     .send()
+//     .await?;
+
+//     dbg! resp
+
+//     println!("saving : {}", descript);
+
+//     if resp.status() != 200 {
+//         println!("failed to submit note");
+//         return Ok(());
+//     }
+
+//     Ok(())
+// }
+
+// async fn query(descript:String) {
+//     println!("querying : {}", descript);
+// }
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+    let client = reqwest::Client::new();
+    let resp = client.get("https://bashfull-server.vercel.app/api/test")
+        .send()
+        .await?.text().await;
+
+    // if resp.status() != 200 {
+    //     println!("failed to submit note");
+    //     return Ok(());
+    // }
+
+
+    println!("{:?}", resp);
+
+    Ok(())
+}    
+
+
+// #[tokio::main]
+// fn main() {
+//     match Bashfull::from_args() {
+//         Bashfull::Save {descript, command} => {
+//             save(descript, command);
+//         },
+//         Bashfull::Query {descript} => {
+//             query(descript);
+//         }
+//         _ => (),
+//     }
+//     //println!("{:?}", opt.Save);
+// }
