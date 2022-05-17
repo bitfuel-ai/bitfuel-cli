0968a1a5077f6e5b00c2112b3df8980e53a2ca17
jacobsolawetz <jacobsolawetz@gmail.com>
2022-05-17 12:59:16 +0000

    rename to bitfuel

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,5 @@
 #/bin/bash
-cross build --target aarch64-linux-android
-cross build --target arm-linux-androideabi
-cross build --target armv7-linux-androideabi
-cross build --target i686-linux-android
-cross build --target x86_64-linux-android
-cross build --target aarch64-apple-ios
-cross build --target armv7-apple-ios
-cross build --target armv7s-apple-ios
-cross build --target i386-apple-ios
-cross build --target 86_64-apple-ios
+cross build --target aarch64-unknown-linux-gnu
 # cross build --target
 # cross build --target
 # cross build --target
