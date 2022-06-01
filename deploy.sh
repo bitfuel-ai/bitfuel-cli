927f3d0fa2b5072327affae01f98ab3b7281057d
Sachin Agarwal <sachin@roboflow.com>
2022-06-01 03:30:04 +0000

    adios rust

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-cross build --target aarch64-unknown-linux-gnu
-# cross build --target
-# cross build --target
-# cross build --target
-# cross build --target
-# cross build --target
-# cross build --target
-
